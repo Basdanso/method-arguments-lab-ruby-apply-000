1a696c665a018066a29a5007c13965a10df09e7a
Basiru Danso <basirudns@gmail.com>
2019-05-01 02:45:03 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,13 +1,13 @@
 
 
-def introduction(name)
-  puts "Hi, my name is #{name}."
-end
+#def introduction(name)
+  #puts "Hi, my name is #{name}."
+#end
 
-puts introduction(Josh)
+#puts introduction(Josh)
 
 def introduction_with_language(name language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-puts introduction_with_language()
+puts introduction_with_language("Dan", "Ember.js")
